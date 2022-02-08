e02df9230074e25e824f88c73f1c5abfc950c66a
annieasryan <msasryan88@gmail.com>
2022-02-08 14:28:27 +0400

    tic-tac-toe

--- src/components/games/ticTacToe/ticTacToe.js
+++ src/components/games/ticTacToe/ticTacToe.js
@@ -1,8 +1,38 @@
+
+import { useState } from 'react';
+import Board from './board';
+import { calculateWinner } from '../../../helpers/helper_tictacwinner'
 import './ticTacToe.css';
 
 
-const TicTacToe = () => (
-    <div className='tic-tac-toe'> </div>
-)
+const TicTacToe = () => {
+    const [board, setBoard] = useState(Array(9).fill(null))
+    const [xIsNext, setXIsNext] = useState(true)
+    const winner = calculateWinner(board)
+
+    const handleButton = (ind) => {
+        const boardCopy = [...board]
+        if (winner || boardCopy[ind]) return
+        boardCopy[ind] = xIsNext ? 'X' : 'O'
+        setBoard(boardCopy)
+        setXIsNext(!xIsNext)
+    }
+    const reStart = () => {
+        setBoard(Array(9).fill(null))
+    }
+
+
+    return (
+
+        <div className='wrapper'>
+            <button className='restart' onClick={reStart}>Restart</button>
+            <Board squares={board} click={handleButton} />
+            <p className='info'>
+                {winner ? 'Winner ' + winner : (xIsNext ? 'Player 1 to move' : 'Player 2 to move')}
+            </p>
+        </div>
+
+    )
+}
 
 export default TicTacToe

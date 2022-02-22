02b353e88e226eaede689ed037f6584fd6a2d447
MariamikGhazaryan <mariamikghazaryan1997@gmail.com>
2022-02-22 12:26:16 +0400

    Score logic.

--- src/components/games/hangman/game-over/game-over.js
+++ src/components/games/hangman/game-over/game-over.js
@@ -1,11 +1,24 @@
 import { useSelector } from "react-redux";
-import { hangManSelector } from "../../../../redux/selectors";
 import "./game-over.css";
 import Restart from "../restart/restart";
+import { hangManSelector, userSelector } from "../../../../redux/selectors";
+import { addScore } from "../../../../helpers/api";
+import { useEffect } from 'react';
 
 export default function GameOver() {
   const { word } = useSelector(hangManSelector);
+  const { guessedLetters } = useSelector(hangManSelector);
+  const { currentUser } = useSelector(userSelector);
 
+  useEffect(() => {
+    const body = {
+      userId: currentUser.id,
+      game: 'HangMan',
+      score: guessedLetters.filter((item) => item).length
+    }
+    addScore(`scores`, body).then()
+  }, []);
+  
   return (
     <div>
       <div className="gameOver">Game Over</div>

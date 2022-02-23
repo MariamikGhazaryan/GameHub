dcbad925521bb022dcf039a1ae6bc8b01a7687b5
MariamikGhazaryan <mariamikghazaryan1997@gmail.com>
2022-02-23 19:22:58 +0400

    Design changes.

--- src/components/please-login/please-login.js
+++ src/components/please-login/please-login.js
@@ -5,7 +5,7 @@ import {Modal} from "../modal/modal";
 export default function PleaseLogin() {
   return (
     <Modal show={true}>
-      Please <a href="../login">Log In</a> for playing a game.
+      Please <a className="url-to-login" href="../login">Log In</a> for playing a game.
     </Modal>
   );
 }
